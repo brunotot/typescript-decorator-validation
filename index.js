dade43d0404d7d0efa242cce4579045b35abeda7
crorisvanjski4 <crorisvanjski4@srce.hr>
2022-12-29 11:31:31 +0100

    index update

---
+++ index.js
@@ -0,0 +1,6 @@
+import InferredType from "./src/constants/InferredType";
+import Rule from "./src/decorators/validators/custom/Rule";
+import ValidatorService from "./src/service/ValidatorService";
+import { validators } from "./src/utils/ValidatorDecoratorUtils";
+
+export { InferredType, ValidatorService, validators, Rule };
