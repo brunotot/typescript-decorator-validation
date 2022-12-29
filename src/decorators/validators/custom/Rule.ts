b7bed769ff1faa45e4d7d09833ae956658c9e6f7
crorisvanjski4 <crorisvanjski4@srce.hr>
2022-12-29 17:59:10 +0100

    refactor Rule

--- src/decorators/validators/custom/Rule.ts
+++ src/decorators/validators/custom/Rule.ts
@@ -1,9 +1,12 @@
 import ValidatorService, {
+	EvaluateValidationType,
 	FieldValidatorBuilderProps,
 } from "../../../service/ValidatorService";
 
 export type RuleProps<T> = FieldValidatorBuilderProps<T>;
 
-export default function Rule<T>(props: RuleProps<T>) {
-	return ValidatorService.buildFieldValidatorDecorator<T>(props);
+export default function Rule<T>(isValid: EvaluateValidationType<T>) {
+	return ValidatorService.buildFieldValidatorDecorator<T>({
+		isValid,
+	});
 }
