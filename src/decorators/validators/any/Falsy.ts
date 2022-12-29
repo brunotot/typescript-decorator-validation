9d89f2fa0929c82dc3f9f1f34525af6017d7ae7c
crorisvanjski4 <crorisvanjski4@srce.hr>
2022-12-29 15:24:16 +0100

    modify truthy and falsy validator

--- src/decorators/validators/any/Falsy.ts
+++ src/decorators/validators/any/Falsy.ts
@@ -1,12 +1,13 @@
 import ValidatorService from "../../../service/ValidatorService";
 import ErrorMessage from "../../../constants/ErrorMessage";
+import { isTruthy } from "./Truthy";
 
 export default function Falsy(message: string = ErrorMessage.Falsy()) {
 	return ValidatorService.buildFieldValidatorDecorator<any>({
 		isValid: (value) => ({
 			key: "Falsy",
 			message,
-			valid: !value,
+			valid: !isTruthy(value),
 		}),
 	});
 }
