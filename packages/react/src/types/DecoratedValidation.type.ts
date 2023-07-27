c7f6786859242b36e23ac68730dd2924a51282d4
Bruno Tot <brunotot10000@gmail.com>
2023-07-27 12:21:58 +0200

    struct refactor

--- packages/react/src/types/DecoratedValidation.type.ts
+++ packages/react/src/types/DecoratedValidation.type.ts
@@ -1,8 +1,9 @@
 import { DetailedErrors, Errors } from "tdv-core";
+import { Dispatch, SetStateAction } from "react";
 
 export type DecoratedValidation<T> = {
   value: T;
-  setValue: React.Dispatch<React.SetStateAction<T>>;
+  setValue: Dispatch<SetStateAction<T>>;
   valid: boolean;
   detailedErrors: DetailedErrors<T>;
   errors: Errors<T>;
