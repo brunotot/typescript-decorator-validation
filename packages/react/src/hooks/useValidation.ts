f456a7522facd8bde175a013c70c7233a43f8b0e
Bruno Tot <brunotot10000@gmail.com>
2023-08-05 15:50:30 +0200

    bump react module patch version

--- packages/react/src/hooks/useValidation.ts
+++ packages/react/src/hooks/useValidation.ts
@@ -1,20 +1,28 @@
 import { useEffect, useMemo, useState } from "react";
-import { DecoratedValidation } from "../types/DecoratedValidation.type";
 import { DetailedErrors, EntityProcessor, Errors, Payload } from "tdv-core";
+import { DecoratedValidation } from "../types/DecoratedValidation.type";
 import { DecoratedValidationProps } from "../types/DecoratedValidationProps.type";
 
 export default function useValidation<T>({
   defaultValue,
   model,
-  groups = []
+  groups = [],
 }: DecoratedValidationProps<T>): DecoratedValidation<T> {
   const processor = useMemo(() => new EntityProcessor<T>(model, ...groups), []);
-  const [value, setValue] = useState<T>(defaultValue ?? processor.buildEmptyInstance());
-  const [detailedErrors, setDetailedErrors] = useState<DetailedErrors<T>>({} as DetailedErrors<T>);
+  const [value, setValue] = useState<T>(
+    defaultValue ?? processor.buildEmptyInstance()
+  );
+  const [detailedErrors, setDetailedErrors] = useState<DetailedErrors<T>>(
+    {} as DetailedErrors<T>
+  );
   const [errors, setErrors] = useState<Errors<T>>({} as Errors<T>);
   const payload = value as Payload<T>;
 
   useEffect(() => {
+    const processorLocal = processor;
+    if (processorLocal) {
+      debugger;
+    }
     setDetailedErrors(processor.getDetailedErrors(payload));
     setErrors(processor.getErrors(payload));
   }, [value]);
