5a9d33c198fcaab3c8820d72704e510f3b310305
Bruno Tot <brunotot10000@gmail.com>
2023-08-28 17:35:02 +0200

    bump react 1.0.86 version

--- packages/react/scripts/deploy.sh
+++ packages/react/scripts/deploy.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
 
-CLR_PREFIX="\033["
-CLR_SUFFIX="m"
-
-CYAN="0;34"
-GREEN="1;32"
-RESET="0"
-GREY="1;30"
-
-color() {
-  local colorCode="${1:-$RESET}"
-  echo "$CLR_PREFIX$colorCode$CLR_SUFFIX"
-}
+. ./scripts/spinner.sh
+. ./scripts/colors.sh
 
 VERSION="$1"
 
-echo -e "$(color $CYAN)1 $(color)/ $(color $GREY)6$(color) Cleaning cache..."
+start "$(color $CYAN)1 $(color)" " $(color $GREY)6$(color) Cleaning cache..."
 npm cache clean --force --silent
+stop "/"
 
-echo -e "$(color $CYAN)2 $(color)/ $(color $GREY)6$(color) Installing dependencies..."
+start "$(color $CYAN)2 $(color)" " $(color $GREY)6$(color) Installing dependencies..."
 npm i --force --silent
+stop "/"
 
-echo -e "$(color $CYAN)3 $(color)/ $(color $GREY)6$(color) Running build script..."
+start "$(color $CYAN)3 $(color)" " $(color $GREY)6$(color) Running build script..."
 npm run build >/dev/null 2>&1
+stop "/"
 
-echo -e "$(color $CYAN)4 $(color)/ $(color $GREY)6$(color) Bumping version to $VERSION..."
+start "$(color $CYAN)4 $(color)" " $(color $GREY)6$(color) Bumping version to $VERSION..."
 UPDATED_VERSION=$(npm version $VERSION --force --silent)
 VERSION=$(echo "$UPDATED_VERSION" | sed '1d' | cut -d 'v' -f 2)
 VERSION_NUMBER="${VERSION#v}"
+stop "/"
 
-echo -e "$(color $CYAN)5 $(color)/ $(color $GREY)6$(color) Publishing package..."
+start "$(color $CYAN)5 $(color)" " $(color $GREY)6$(color) Publishing package..."
 npm publish --access=public --silent
+stop "/"
 
-echo -e "$(color $GREEN)6 $(color)/ $(color $GREEN)6 $(color)Pushing changes..."
+start "$(color $GREEN)6 $(color)" " $(color $GREEN)6 $(color)Pushing changes..."
 git add .
 git commit -m "bump react $VERSION version" --quiet -u
 git push --quiet -u --no-progress >/dev/null 2>&1
+stop "/"
 
 echo -e "\n    🚀 $(color $GREEN)Done!$(color)\n    ⭐ v$VERSION_NUMBER\n"
--- packages/react/scripts/test.sh
+++ packages/react/scripts/test.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-CLR_PREFIX="\033["
-CLR_SUFFIX="m"
+. ./scripts/spinner.sh
+. ./scripts/colors.sh
 
-CYAN="0;34"
-GREEN="1;32"
-RESET="0"
-GREY="1;30"
-
-color() {
-  local colorCode="${1:-$RESET}"
-  echo "$CLR_PREFIX$colorCode$CLR_SUFFIX"
-}
-
-echo -e "$(color $CYAN)1 $(color)/ $(color $GREY)3$(color) Setting environment variables..."
+start "$(color $CYAN)1 $(color)" " $(color $GREY)3$(color) Setting environment variables..."
 export TS_JEST_DISABLE_VER_CHECKER=true >/dev/null 2>&1
+stop "/"
 
-echo -e "$(color $CYAN)2 $(color)/ $(color $GREY)3$(color) Installing dependencies..."
+start "$(color $CYAN)2 $(color)" " $(color $GREY)3$(color) Installing dependencies..."
 npm i --silent --force >/dev/null 2>&1
+stop "/"
 
-echo -e "$(color $CYAN)3 $(color)/ $(color $GREY)3$(color) Running test suites..."
-jest
+echo -e "\n$(color $CYAN)3 $(color)/ $(color $GREY)3$(color) Running test suites..."
+npx jest
