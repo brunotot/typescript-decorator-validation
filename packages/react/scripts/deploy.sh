f65648a98cf45ae98be6f192bcdefe6a560e9b9e
Bruno Tot <brunotot10000@gmail.com>
2023-08-28 14:48:26 +0200

    bump react $VERSION version

---
+++ packages/react/scripts/deploy.sh
@@ -0,0 +1,23 @@
+VERSION="$1"
+
+## "deploy:patch": "echo '\u001b[32m1/6\u001b[0m Cleaning cache...' && npm cache clean --force --silent && echo '\u001b[32m2/6\u001b[0m Installing dependencies...' && npm i --force --silent && echo '\u001b[32m3/6\u001b[0m Running build script...' && npm run build && echo '\u001b[32m4/6\u001b[0m Bumping version to PATCH...' && npm version patch --force --silent && echo '\u001b[32m5/6\u001b[0m Publishing package...' && npm publish --access=public --silent && echo '\u001b[32m6/6\u001b[0m Pushing changes...' && git add . && git commit -m 'bump react patch version' && git push",
+
+echo '\u001b[32m1/6\u001b[0m Cleaning cache...'
+npm cache clean --force --silent
+
+echo '\u001b[32m2/6\u001b[0m Installing dependencies...'
+npm i --force --silent
+
+echo '\u001b[32m3/6\u001b[0m Running build script...'
+npm run build
+
+echo "\u001b[32m4/6\u001b[0m Bumping version to $VERSION..."
+npm version $VERSION --force --silent
+
+echo '\u001b[32m5/6\u001b[0m Publishing package...'
+npm publish --access=public --silent
+
+echo '\u001b[32m6/6\u001b[0m Pushing changes...'
+git add .
+git commit -m 'bump react $VERSION version'
+git push
