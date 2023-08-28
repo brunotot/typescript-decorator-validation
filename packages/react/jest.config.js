41c7f72a6d515158ba1683c449ff4a51c36e9e49
Bruno Tot <brunotot10000@gmail.com>
2023-08-28 14:39:55 +0200

    bump react patch version

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  testMatch: ["**/*.test.ts", "**/*.test.tsx"],
+  transformIgnorePatterns: ["./node_modules/", "./dist/"],
 };
