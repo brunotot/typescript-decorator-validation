0156e1ab4ba99bb91976eedc19ee230b75d0cb54
Bruno Tot <brunotot10000@gmail.com>
2023-08-28 18:48:59 +0200

    bump react 1.0.94 version

--- packages/react/scripts/test.sh
+++ packages/react/scripts/test.sh
@@ -3,13 +3,6 @@
 . ./scripts/spinner.sh
 . ./scripts/colors.sh
 
-start "$(color $CYAN)1 $(color)" " $(color $GREY)3$(color) Setting environment variables..."
 export TS_JEST_DISABLE_VER_CHECKER=true >/dev/null 2>&1
-stop "/"
-
-start "$(color $CYAN)2 $(color)" " $(color $GREY)3$(color) Installing dependencies..."
 npm i --silent --force >/dev/null 2>&1
-stop "/"
-
-echo -e "\n$(color $CYAN)3 $(color)/ $(color $GREY)3$(color) Running test suites..."
-npx jest
+jest
