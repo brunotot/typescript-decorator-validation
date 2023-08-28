84b4b0079263a2bc07ed7ba36925ba82aadccb0d
Bruno Tot <brunotot10000@gmail.com>
2023-08-28 18:40:38 +0200

    bump react 1.0.91 version

--- packages/react/scripts/deploy.sh
+++ packages/react/scripts/deploy.sh
@@ -5,29 +5,33 @@
 
 VERSION="$1"
 
-start "$(color $CYAN)1 $(color)" " $(color $GREY)6$(color) Cleaning cache..."
+start "$(color $CYAN)1 $(color)" " $(color $GREY)7$(color) Cleaning cache..."
 npm cache clean --force --silent
 stop "/"
 
-start "$(color $CYAN)2 $(color)" " $(color $GREY)6$(color) Installing dependencies..."
+start "$(color $CYAN)2 $(color)" " $(color $GREY)7$(color) Installing dependencies..."
 npm i --force --silent --no-progress
 stop "/"
 
-start "$(color $CYAN)3 $(color)" " $(color $GREY)6$(color) Running build script..."
+start "$(color $CYAN)3 $(color)" " $(color $GREY)7$(color) Running tests..."
+npm run test --silent >/dev/null 2>&1
+stop "/"
+
+start "$(color $CYAN)4 $(color)" " $(color $GREY)7$(color) Running build script..."
 npm run build >/dev/null 2>&1
 stop "/"
 
-start "$(color $CYAN)4 $(color)" " $(color $GREY)6$(color) Bumping version to $VERSION..."
+start "$(color $CYAN)5 $(color)" " $(color $GREY)7$(color) Bumping version to $VERSION..."
 UPDATED_VERSION=$(npm version $VERSION --force --silent --no-progress)
 VERSION=$(echo "$UPDATED_VERSION" | sed '1d' | cut -d 'v' -f 2)
 VERSION_NUMBER="${VERSION#v}"
 stop "/"
 
-start "$(color $CYAN)5 $(color)" " $(color $GREY)6$(color) Publishing package..."
+start "$(color $CYAN)6 $(color)" " $(color $GREY)7$(color) Publishing package..."
 npm publish --access=public --silent
 stop "/"
 
-start "$(color $GREEN)6 $(color)" " $(color $GREEN)6 $(color)Pushing changes..."
+start "$(color $GREEN)7 $(color)" " $(color $GREEN)7 $(color)Pushing changes..."
 git add .
 git commit -m "bump react $VERSION version" --quiet -u
 git push --quiet -u --no-progress >/dev/null 2>&1
