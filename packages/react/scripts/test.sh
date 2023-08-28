b0dd665000cf15c6d2e72488595fd19f1e5e0884
Bruno Tot <brunotot10000@gmail.com>
2023-08-28 23:04:59 +0200

    bump react 1.1.0 version

--- packages/react/scripts/test.sh
+++ packages/react/scripts/test.sh
@@ -5,7 +5,6 @@
 
 export TS_JEST_DISABLE_VER_CHECKER=true >/dev/null 2>&1
 npm i --silent --force >/dev/null 2>&1
-npx jest --noStackTrace --silent  # --testResultsProcessor ./tests/customFormatter.js
-echo -e "\r\r\r\r"
+npx jest --noStackTrace --silent
 EXIT_STATUS=$?
 exit $EXIT_STATUS
