fcaccef6cbd24f49e7b4000ecb042d0191004f80
Bruno Tot <brunotot10000@gmail.com>
2023-08-28 23:45:07 +0200

    struct refactor

---
+++ scripts/colors.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CLR_PREFIX="\033["
+CLR_SUFFIX="m"
+
+CYAN="0;34"
+GREEN="1;32"
+RESET="0"
+GREY="1;30"
+RED="1;31"
+
+color() {
+  local colorCode="${1:-$RESET}"
+  echo "$CLR_PREFIX$colorCode$CLR_SUFFIX"
+}
---
+++ scripts/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+. ./../../scripts/spinner.sh
+. ./../../scripts/colors.sh
+
+export TS_JEST_DISABLE_VER_CHECKER=true >/dev/null 2>&1
+npm i --silent --force >/dev/null 2>&1
+npx jest --noStackTrace --silent --detectOpenHandles
+EXIT_STATUS=$?
+exit $EXIT_STATUS
