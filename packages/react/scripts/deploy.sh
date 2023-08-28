89585cc1dedfb61181a832c81638023718ec62c6
Bruno Tot <brunotot10000@gmail.com>
2023-08-28 15:02:13 +0200

    bump react patch version

--- packages/react/scripts/deploy.sh
+++ packages/react/scripts/deploy.sh
@@ -2,8 +2,8 @@
 
 VERSION="$1"
 
-CLR_PREFIX="\[\033["
-CLR_SUFFIX="m\]"
+CLR_PREFIX="\033[m"
+CLR_SUFFIX="m"
 
 GREEN="1;32"
 RESET="0"
@@ -13,22 +13,22 @@ clr() {
   echo "$CLR_PREFIX$colorCode$CLR_SUFFIX"
 }
 
-echo -e "$(clr GREEN)1/6$(clr) Cleaning cache..."
+echo -e "$(clr $GREEN)1/6$(clr) Cleaning cache..."
 npm cache clean --force --silent
 
-echo -e "$(clr GREEN)2/6$(clr) Installing dependencies..."
+echo -e "$(clr $GREEN)2/6$(clr) Installing dependencies..."
 npm i --force --silent
 
-echo -e "$(clr GREEN)3/6$(clr) Running build script..."
+echo -e "$(clr $GREEN)3/6$(clr) Running build script..."
 npm run build
 
-echo -e "$(clr GREEN)4/6$(clr) Bumping version to $VERSION..."
+echo -e "$(clr $GREEN)4/6$(clr) Bumping version to $VERSION..."
 npm version $VERSION --force --silent
 
-echo -e "$(clr GREEN)5/6$(clr) Publishing package..."
+echo -e "$(clr $GREEN)5/6$(clr) Publishing package..."
 npm publish --access=public --silent
 
-echo -e "$(clr GREEN)6/6$(clr) Pushing changes..."
+echo -e "$(clr $GREEN)6/6$(clr) Pushing changes..."
 git add .
 git commit -m "bump react $VERSION version"
 git push
