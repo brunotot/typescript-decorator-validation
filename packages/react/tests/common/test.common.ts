0829777476eb415fb3fe296d7b38c48bdf985d64
Bruno Tot <brunotot10000@gmail.com>
2023-08-28 13:05:19 +0200

    struct refactor

---
+++ packages/react/tests/common/test.common.ts
@@ -0,0 +1,5 @@
+export function id(key: string) {
+  return {
+    "data-testid": key,
+  };
+}
---
+++ packages/react/tests/impl/useValidation/useValidation.test.tsx
@@ -0,0 +1,34 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { validators } from "tdv-core";
+import useValidation from "../../../src/hooks/useValidation";
+
+jest.spyOn(console, "error").mockImplementation(() => {});
+
+class TestForm {
+  @validators.string.Required()
+  username!: string;
+}
+
+test("first render error, second render success", async () => {
+  const { result, waitForNextUpdate } = renderHook(
+    () =>
+      useValidation({
+        model: TestForm,
+      }),
+    {
+      initialProps: undefined,
+      wrapper: undefined,
+    }
+  );
+
+  expect(result.current).toHaveProperty("errors.username");
+  expect(result.current.errors.username).toHaveLength(1);
+
+  result.current.setForm({
+    username: "value",
+  });
+
+  await waitForNextUpdate();
+
+  expect(result.current.errors.username).toHaveLength(0);
+});
