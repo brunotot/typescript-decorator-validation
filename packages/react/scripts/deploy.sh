6489e2bb0ea38a00ac359d3a0e037d39b9526080
Bruno Tot <brunotot10000@gmail.com>
2023-08-28 14:59:51 +0200

    bump react patch version

--- packages/react/scripts/deploy.sh
+++ packages/react/scripts/deploy.sh
@@ -1,23 +1,36 @@
+#!/bin/bash
+
 VERSION="$1"
 
-## "deploy:patch": "echo '\u001b[32m1/6\u001b[0m Cleaning cache...' && npm cache clean --force --silent && echo '\u001b[32m2/6\u001b[0m Installing dependencies...' && npm i --force --silent && echo '\u001b[32m3/6\u001b[0m Running build script...' && npm run build && echo '\u001b[32m4/6\u001b[0m Bumping version to PATCH...' && npm version patch --force --silent && echo '\u001b[32m5/6\u001b[0m Publishing package...' && npm publish --access=public --silent && echo '\u001b[32m6/6\u001b[0m Pushing changes...' && git add . && git commit -m 'bump react patch version' && git push",
+CLR_PREFIX="\033[m"
+CLR_SUFFIX="m"
 
-echo "\u001b[32m1/6\u001b[0m Cleaning cache..."
+GREEN="1;32"
+RESET="0"
+
+clr() {
+  local colorCode="${1:-$RESET}"
+  echo "$CLR_PREFIX$colorCode$CLR_SUFFIX"
+}
+
+result=$(clr GREEN)
+
+echo -e "$(clr GREEN)1/6$(clr) Cleaning cache..."
 npm cache clean --force --silent
 
-echo "\u001b[32m2/6\u001b[0m Installing dependencies..."
+echo -e "$(clr GREEN)2/6$(clr) Installing dependencies..."
 npm i --force --silent
 
-echo "\u001b[32m3/6\u001b[0m Running build script..."
+echo -e "$(clr GREEN)3/6$(clr) Running build script..."
 npm run build
 
-echo "\u001b[32m4/6\u001b[0m Bumping version to $VERSION..."
+echo -e "$(clr GREEN)4/6$(clr) Bumping version to $VERSION..."
 npm version $VERSION --force --silent
 
-echo "\u001b[32m5/6\u001b[0m Publishing package..."
+echo -e "$(clr GREEN)5/6$(clr) Publishing package..."
 npm publish --access=public --silent
 
-echo "\u001b[32m6/6\u001b[0m Pushing changes..."
+echo -e "$(clr GREEN)6/6$(clr) Pushing changes..."
 git add .
 git commit -m "bump react $VERSION version"
 git push
