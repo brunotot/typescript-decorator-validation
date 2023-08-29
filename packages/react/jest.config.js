d70aa5f23b3247be9002ff98aea5196245ac2086
Bruno Tot <brunotot10000@gmail.com>
2023-08-29 18:51:17 +0200

    bump react 1.1.2 version

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/*.test.tsx"],
+  testEnvironment: "jsdom",
+  testMatch: ["**/*.test.ts", "**/*.test.tsx"],
   transformIgnorePatterns: ["./node_modules/", "./dist/"],
   reporters: ["<rootDir>/../../reporter.js"],
 };
