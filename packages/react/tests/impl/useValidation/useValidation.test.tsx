124a7b73cde851c9a76cd375e8e37cff2e098691
Bruno Tot <brunotot10000@gmail.com>
2023-08-30 22:04:23 +0200

    struct refactor

--- packages/react/tests/impl/useValidation/useValidation.test.tsx
+++ packages/react/tests/impl/useValidation/useValidation.test.tsx
@@ -11,25 +11,22 @@ class TestForm {
 }
 
 test("first render error, second render success", async () => {
-  const { result, waitForNextUpdate } = renderHook(
-    () =>
-      useValidation({
-        model: TestForm,
-      }),
-    {
-      initialProps: undefined,
-      wrapper: undefined,
-    }
+  const { result, waitForNextUpdate } = renderHook(() =>
+    useValidation({
+      model: TestForm,
+    })
   );
 
-  expect(result.current).toHaveProperty("errors.username");
-  expect(result.current.errors.username).toHaveLength(1);
+  const hook = result.current;
 
-  result.current.setForm({
+  expect(hook).toHaveProperty("errors.username");
+  expect(hook.errors.username).toHaveLength(1);
+
+  hook.setForm({
     username: "value",
   });
 
   await waitForNextUpdate();
 
-  expect(result.current.errors.username).toHaveLength(0);
+  expect(hook.errors.username).toHaveLength(0);
 });
