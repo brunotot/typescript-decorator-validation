c73c22b6467f14f285fd626a993b0c1b51a09b2c
Bruno Tot <brunotot10000@gmail.com>
2023-09-14 12:39:47 +0200

    ✨ [feat]: Add support for german, spanish and french localization

--- packages/core/src/messages/model/messages.ts
+++ packages/core/src/messages/model/messages.ts
@@ -1,5 +1,8 @@
 import { Locale } from "../message.types";
+import * as MessageFormatDe from "../translations/de.json";
 import * as MessageFormatEn from "../translations/en.json";
+import * as MessageFormatEs from "../translations/es.json";
+import * as MessageFormatFr from "../translations/fr.json";
 import * as MessageFormatHr from "../translations/hr.json";
 
 export type LocalizedMessages = Record<Locale, Messages>;
@@ -9,6 +12,9 @@ export type Messages = Record<string, string>;
 const localizedMessages: LocalizedMessages = {
   en: MessageFormatEn,
   hr: MessageFormatHr,
+  de: MessageFormatDe,
+  es: MessageFormatEs,
+  fr: MessageFormatFr,
 };
 
 export default localizedMessages;
