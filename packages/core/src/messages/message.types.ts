1d4196a4d6836d2abb4dbb9575e099b6dc95404b
Bruno Tot <brunotot10000@gmail.com>
2023-09-15 10:25:29 +0200

    🔄 [chore]: Automated commit

--- packages/core/src/messages/message.types.ts
+++ packages/core/src/messages/message.types.ts
@@ -1,3 +1,7 @@
 export type Locale = "hr" | "en" | "de" | "es" | "fr" | "it" | "nl";
 
 export type LocaleDefault = "en";
+
+export type LocalizedMessages = Record<Locale, Messages>;
+
+export type Messages = Record<string, string>;
--- packages/core/src/messages/model/messages.ts
+++ packages/core/src/messages/model/messages.ts
@@ -1,4 +1,4 @@
-import { Locale } from "../message.types";
+import { LocalizedMessages } from "../message.types";
 import * as MessageFormatDe from "../translations/de.json";
 import * as MessageFormatEn from "../translations/en.json";
 import * as MessageFormatEs from "../translations/es.json";
@@ -6,10 +6,6 @@ import * as MessageFormatFr from "../translations/fr.json";
 import * as MessageFormatHr from "../translations/hr.json";
 import * as MessageFormatIt from "../translations/it.json";
 import * as MessageFormatNl from "../translations/nl.json";
-
-export type LocalizedMessages = Record<Locale, Messages>;
-
-export type Messages = Record<string, string>;
 
 const localizedMessages: LocalizedMessages = {
   en: MessageFormatEn,
