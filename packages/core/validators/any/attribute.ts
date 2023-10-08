2022ad1ea2517030425170f863e824e1a261dc2c
Bruno Tot <brunotot10000@gmail.com>
2023-10-08 10:25:03 +0200

    🔄 [chore]: Automated commit

--- packages/core/validators/any/attribute.ts
+++ packages/core/validators/any/attribute.ts
@@ -3,7 +3,7 @@ import { Decorator, Overrides, Utilities } from "api";
 /**
  * Creates a decorator which flags the given field as a non-primitive (will validate inner fields of `T`).
  *
- * If a field which is being decorated is not a {@link API.Utilities.Types.Primitive primitive}
+ * If a field which is being decorated is not a {@link Utilities.Types.Primitive primitive}
  * (`string`, `number`, `boolean`, `bigint`, `Date`) and isn't marked as a primitive in {@link Overrides.PrimitiveSet overrides} interface
  * then the framework treats it as a custom, client-defined validable class. That having in mind, you will always want to apply `@attribute`
  * to those types of fields so the runtime evaluation matches the TypeScript compiler type evaluation. For more clarity check examples below.
