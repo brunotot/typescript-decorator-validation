143572a72525305b43c423bf34c8274884e5f285
Bruno Tot <brunotot10000@gmail.com>
2023-10-17 01:38:59 +0200

    📝 [docs]: Finalize documentation for TranslationService.translate

--- packages/core/src/localization/service/TranslationService.ts
+++ packages/core/src/localization/service/TranslationService.ts
@@ -3,20 +3,30 @@ import API from "api";
 import MessageReaderService from "./MessageReaderService";
 
 /**
- * Localizes a string based on a key and optional arguments.
+ * Localizes a string based on a corresponding key and optional arguments mapped by indices. (ex: `"Hello {0}! How are you?"`)
  *
- * @param key - The key corresponding to the localized string.
- * @param args - Optional values to replace placeholders in the localized string.
- * @returns The localized and formatted string.
+ * @param key - The key corresponding to the localized string. (ex: `"Hello"`)
+ * @param args - Optional values to replace template placeholders in the localized string. (ex: `["John Doe"]`)
+ * @remarks This translation mechanism utilizes {@link API.Utilities.Strings.sprintf sprintf} function for replacing template placeholders with the provided `args`.
+ * @returns The localized and formatted message by `key` and `args` params.
  *
  * @example
+ * 1: Basic usage (locale is evaluated by `LocaleResolver.getLocale()` which defaults to `"en"`)
  * ```typescript
- * const greeting = translate("hello", "Bruno");  // Output might be: "Hello, Bruno!"
+ * const greeting = translate(null, "Hello");  // "Hello {0}! How are you?"
  * ```
  *
- * @remarks
- * The function fetches the current locale using `getLocale()` and retrieves the corresponding message from `localeMessages`.
- * It then uses `sprintf` to replace any placeholders in the message with the provided `args`.
+ * @example
+ * 2: Supplying a custom locale
+ * ```typescript
+ * const greeting = translate("es", "Hello");  // "¡Hola {0}! ¿Cómo estás?"
+ * ```
+ *
+ * @example
+ * 3: Supplying template arguments
+ * ```typescript
+ * const greeting = translate("en", "Hello", "John Doe");  // "Hello John Doe! How are you?"
+ * ```
  */
 export function translate(
   locale: API.Localization.Resolver.LocaleResolver.Locale | null,
