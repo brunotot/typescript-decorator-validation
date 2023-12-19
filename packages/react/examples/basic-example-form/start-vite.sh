d80079cfa16ad62c4ce8b8aa9d0c99958b0db135
Bruno Tot <brunotot10000@gmail.com>
2023-12-19 16:04:49 +0100

    ♻️ [refactor]: Rewrite vite start script

--- packages/react/examples/basic-example-form/start-vite.sh
+++ packages/react/examples/basic-example-form/start-vite.sh
@@ -13,9 +13,8 @@ start "$(color $CYAN)1 $(color)" " $(color $GREY)8$(color) Cleaning $(color $CYA
 rm -rf ../../../core/dist
 stop "/"
 
-
 start "$(color $CYAN)2 $(color)" " $(color $GREY)8$(color) Installing $(color $CYAN)tdv-core$(color) dependencies"
-npm i  --no-progress --prefix=../../../core --silent
+npm i --no-progress --prefix=../../../core --silent --force
 stop "/"
 
 start "$(color $CYAN)3 $(color)" " $(color $GREY)8$(color) Building $(color $CYAN)tdv-core$(color) package"
@@ -29,7 +28,8 @@ rm -rf ../../../react/dist
 stop "/"
 
 start "$(color $CYAN)5 $(color)" " $(color $GREY)8$(color) Installing $(color $CYAN)tdv-react$(color) dependencies"
-npm i  --no-progress --prefix=../../../react --silent
+npm i --no-progress --prefix=../../../react --silent --force
+rm -rf ../../node_modules/tdv-core
 stop "/"
 
 start "$(color $CYAN)6 $(color)" " $(color $GREY)8$(color) Building $(color $CYAN)tdv-react$(color) package"
@@ -43,9 +43,11 @@ rm -rf node_modules/.vite
 stop "/"
 
 start "$(color $CYAN)8 $(color)" " $(color $GREY)8$(color) Installing local dependencies"
-npm i  --no-progress --silent
+npm i --no-progress --silent --force
 stop "/"
 
 echo -e "\n\n🚀 Optimizations finished, starting $(color $GREEN)vite$(color)...\n"
 
 npm run build --silent && vite
+
+# rm -rf ../../../core/dist && npm i --no-progress --prefix=../../../core --silent --force && npm run build:noTest --prefix=../../../core --silent && rm -rf ../../../react/dist && npm i --no-progress --prefix=../../../react --silent --force && rm -rf ../../node_modules/tdv-core && npm run build:noTest --prefix=../../../react --silent && rm -rf node_modules/.vite && npm i --no-progress --silent --force && npm run build --silent && vite
